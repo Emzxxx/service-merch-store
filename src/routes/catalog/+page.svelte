3e8882adf5418b33208434c6c5a3fea5d0e81c8e
kcthegreat18 <kfabriol1@up.edu.ph>
2026-03-30 11:53:29 +0800

    feat: get products from supabase + initialize supabase

--- src/routes/catalog/+page.svelte
+++ src/routes/catalog/+page.svelte
@@ -1,39 +1,21 @@
 <script lang="ts">
-    import Card from "$lib/components/Card/Card.svelte";
-    import AdvanceSearch from "$lib/components/AdvanceSearch/advance-search.svelte";
+	import Card from "$lib/components/Card/Card.svelte";
+	import AdvanceSearch from "$lib/components/AdvanceSearch/advance-search.svelte";
 
-    class Product {
-        image: string | null;
-        price: number;
-        name: string;
-
-        constructor(image: string | null, price: number, name: string) {
-            this.image = image;
-            this.price = price;
-            this.name = name;
-        }
-    }
-
-    // populate products with data from Supabase in the future
-    let products = [
-        new Product(null, 199.99, "Product 1"),
-        new Product(null, 299.99, "Product 2"),
-        new Product(null, 399.99, "Product 3"),
-        new Product(null, 499.99, "Product 4"),
-        new Product("/lorem.ipsum", 1, "Product 5")
-    ];
+	let { data } = $props();
+	$effect(() => {
+		console.log('data.products:', data.products);
+	});
 </script>
 
 <div class="flex px-5">
-    <!-- Sording Sidebar -->
-    <div class="w-80 flex-shrink-0">
-        <AdvanceSearch />
-    </div>
+	<div class="w-80 flex-shrink-0">
+		<AdvanceSearch />
+	</div>
 
-    <!-- Catalog Grid -->
-    <div class="grid grid-cols-4 px-5 mx-5">
-        {#each products as product}
-            <Card image={product.image} price={product.price} name={product.name} />
-        {/each}
-    </div>
+	<div class="grid grid-cols-4 px-5 mx-5">
+		{#each data.products as product}
+			<Card image={product.image} price={product.price} name={product.name} />
+		{/each}
+	</div>
 </div>
